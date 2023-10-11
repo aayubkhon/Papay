9e722f5108f1881b6d41a9e68faf3c057fb6832d
aayubxon701 <aayubxon701@gmail.com>
2023-10-11 19:32:40 +0900

    loyhani shakilantirip olish

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("Web server is Started");
+const express = require("express");
+const app = express();
+
+// Mongo db ga kirish
+const db = require("./server").db();
+const mongodb = require("mongodb");
+
+// 1 kirish codelar
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2 Sesseion code
+// 3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+// Post malumotni ozi bilan birga olip keladi va Date base ga yozadi
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const mongodb = require("mongodb");
+
+const connectionString =
+  "mongodb+srv://leo:7810720leo@cluster0.weqsmcn.mongodb.net/Papays";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDb");
+    else {
+      console.log("MongoDb connection secceed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3009;
+      server.listen(PORT, function () {
+        console.log(`The server on port: ${PORT}, http://localhost:${PORT}`);
+      });
+    }
+  }
+);
