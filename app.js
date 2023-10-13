905c0cdd31b52369ac400e2836c5021e8ea64947
aayubxon701 <aayubxon701@gmail.com>
2023-10-13 23:46:35 +0900

    feat:mongoose and schema is qurildi

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 console.log("Web server is Started");
 const express = require("express");
 const app = express();
-const router = require("./router")
-
-// Mongo db ga kirish
-const db = require("./server").db();
-const mongodb = require("mongodb");
+const router = require("./router");
 
 // 1 kirish codelar
 app.use(express.static("public"));
@@ -19,8 +15,7 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 
-app.use("/",router)
+app.use("/", router);
 // Post malumotni ozi bilan birga olip keladi va Date base ga yozadi
-
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 const http = require("http");
-const mongodb = require("mongodb");
+const mongoose = require('mongoose')
 
 const connectionString = process.env.MONGO_URL;
-
-mongodb.connect(
+mongoose.connect(
   connectionString,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
-  (err, client) => {
+  (err, goose) => {
     if (err) console.log("ERROR on connection MongoDb");
     else {
       console.log("MongoDb connection secceed");
-      module.exports = client;
       const app = require("./app");
       const server = http.createServer(app);
       let PORT = process.env.PORT || 3009;
