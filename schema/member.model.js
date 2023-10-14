f3d3226a24cf95afeac03799cac5911f610b4224
aayubxon701 <aayubxon701@gmail.com>
2023-10-14 19:42:02 +0900

    fix:modify memberschema model

--- schema/member.model.js
+++ schema/member.model.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
-const { member_type_enums, member_status_enums } = require("../lib/config");
+const {
+  member_type_enums,
+  member_status_enums,
+  member_ordernary_enums,
+} = require("../lib/config");
 
 const memberSchema = new mongoose.Schema({
   mb_nick: {
@@ -34,11 +38,11 @@ const memberSchema = new mongoose.Schema({
       message: "{VALUE} is not among prmitted lues",
     },
   },
-  mb_full_name: {
+  mb_adress: {
     type: String,
     required: false,
   },
-  mb_adress: {
+  mb_description: {
     type: String,
     required: false,
   },
@@ -51,6 +55,36 @@ const memberSchema = new mongoose.Schema({
     required: false,
     default: 0,
   },
+  mb_top: {
+    type: String,
+    required: false,
+    default: "N",
+    enum: {
+      values: member_ordernary_enums,
+      message: "{VALUE} is not among prmitted lues",
+    },
+  },
+  mb_views: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  mb_likes: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  mb_follow_cnt: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  mb_subscribe_cnt: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  timestamps: true, // createdAt,updateAt
 });
 
-module.exports.module("Member",memberSchema)
+module.exports.module("Member", memberSchema);
