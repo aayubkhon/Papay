986c8c5206980dbba75ca446587b068c939e718e
aayubxon701 <aayubxon701@gmail.com>
2023-10-17 20:43:23 +0900

    feat: eanable secure fature via bcrypt

--- models/Member.js
+++ models/Member.js
@@ -1,29 +1,32 @@
 const Definer = require("../lib/mistake");
 const MemberModel = require("../schema/member.model");
 const assert = require("assert");
+const bcrypt = require("bcrypt");
 class Member {
   constructor() {
     this.memberModel = MemberModel;
   }
 
-  // async signupDate(input) {
-  //   try {
-  //     const new_member = new this.memberModel(input);
+  async signupDate(input) {
+    try {
+      const salt = await bcrypt.genSalt();
+      input.mb_password = await bcrypt.hash(input.mb_password, salt);
+      const new_member = new this.memberModel(input);
 
-  //     let result;
-  //     try {
-  //       result = await new_member.save();
-  //     } catch (mongo_err) {
-  //       console.log(mongo_err);
-  //       throw new Error(Definer.auth_err1);
-  //     }
+      let result;
+      try {
+        result = await new_member.save();
+      } catch (mongo_err) {
+        console.log(mongo_err);
+        throw new Error(Definer.auth_err1);
+      }
 
-  //     result.mb_password = "";
-  //     return result;
-  //   } catch (err) {
-  //     throw err;
-  //   }
-  // }
+      result.mb_password = "";
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
 
   async loginData(input) {
     try {
@@ -32,10 +35,13 @@ class Member {
         .exec();
       assert.ok(member, Definer.auth_err2);
 
-      const isMatch = input.mb_password === member.mb_password;
+      const isMatch = await bcrypt.compare(
+        input.mb_password,
+        member.mb_password
+      );
       assert.ok(isMatch, Definer.auth_err3);
 
-      return await this.memberModel.findOne({mb_nick: input.mb_nick,}).exec()
+      return await this.memberModel.findOne({ mb_nick: input.mb_nick }).exec();
     } catch (err) {
       throw err;
     }
