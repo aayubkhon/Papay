5474d09a6138ac4eb78fb9cb20213775d0a9b492
aayubxon701 <aayubxon701@gmail.com>
2023-10-19 21:10:01 +0900

    feat: ejs framework orqali front endni boshlash

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log("Web server is Started");
 const express = require("express");
 const app = express();
 const router = require("./router");
+const router_bssr = require("./router_bssr");
 
 // 1 kirish codelar
 app.use(express.static("public"));
@@ -14,7 +15,7 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing coded
-// app.use("/resto", router_bssr); // ananaviy yol admin bilan ishlaydi 
+app.use("/resto", router_bssr); // ananaviy yol admin bilan ishlaydi 
 app.use("/", router);  /// rest ap i React frontend bilan ishlaydi
 // Post malumotni ozi bilan birga olip keladi va Date base ga yozadi
 
