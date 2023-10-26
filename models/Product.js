c7cf8ed2d154c2cc5c0c2e695b24ff227a60ad3b
aayubxon701 <aayubxon701@gmail.com>
2023-10-26 21:21:54 +0900

    feat: develop restaurants users editing their products

--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,42 @@
-const { shapeIntoMongooseObjectId } = require('../lib/config');
-const Definer = require('../lib/mistake');
-const ProductModel = require('../schema/product.model')
+const { exec } = require("child_process");
+const { shapeIntoMongooseObjectId } = require("../lib/config");
+const Definer = require("../lib/mistake");
+const ProductModel = require("../schema/product.model");
 const assert = require("assert");
 
 class Product {
-   constructor(){
-      this.productModel = ProductModel
-   }
-  async addNewProductData(data,member) {
-   try {
-      data.restaurant_mb_id = shapeIntoMongooseObjectId(member._id)
+  constructor() {
+    this.productModel = ProductModel;
+  }
+  async addNewProductData(data, member) {
+    try {
+      data.restaurant_mb_id = shapeIntoMongooseObjectId(member._id);
       console.log(data);
-      const new_product = new this.productModel(data)
-      const result = await new_product.save()
-      assert.ok(result,Definer.product_err1)
-      return result
-   } catch (err) {
-      throw err
-   }
-   }
-} 
-module.exports = Product
+      const new_product = new this.productModel(data);
+      const result = await new_product.save();
+      assert.ok(result, Definer.product_err1);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async updateChosenProductData(id, update_data, mb_id) {
+    try {
+      id = shapeIntoMongooseObjectId(id);
+      mb_id = shapeIntoMongooseObjectId(mb_id);
+      const result = await this.productModel
+        .findOneAndUpdate({ _id: id, restaurant_mb_id: mb_id }, update_data, {
+          runValidators: true,
+          lean: true,
+          returnDocument: "after",
+        })
+        .exec();
+      assert.ok(result, Definer.general_err1);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+module.exports = Product;
