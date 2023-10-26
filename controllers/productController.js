40609e09f056a0b630c83a974a3c110f36a507d2
aayubxon701 <aayubxon701@gmail.com>
2023-10-26 20:36:53 +0900

    feat:yangi restoranlar product hosil qilishi

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,7 @@
+const Product = require("../models/Product");
+const assert = require("assert");
+const Definer = require("../lib/mistake");
+
 let productController = module.exports;
 
 productController.getAllProducts = async (req, res) => {
@@ -12,9 +16,18 @@ productController.getAllProducts = async (req, res) => {
 productController.addNewProduct = async (req, res) => {
   try {
     console.log("POST: cont/addNewProduct");
+    assert(req.files, Definer.general_err3);
+    const product = new Product();
+    let data = req.body;
+    data.product_images = req.files.map((ele) => {
+      return ele.path;
+    });
+    const result = await product.addNewProductData(data, req.member);
 
-    //TODO: creation develop
-    res.send("ok");
+    const html = `<script>alert(new dish add successfully);
+                  window.location.replace('resto/products/menu')
+                 </script>`;
+    res.end(html);
   } catch (err) {
     console.log(`ERROR, cont/addNewProduct, ${err.message} `);
   }
@@ -27,4 +40,3 @@ productController.updateChosenProduct = async (req, res) => {
     console.log(`ERROR, cont/updateChosenProduct, ${err.message} `);
   }
 };
-
