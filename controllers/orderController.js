adedaefdf8321d4d7657ad4698aa9762453c5165
aayubxon701 <aayubxon701@gmail.com>
2023-12-09 08:54:27 +0900

    feat:fulfil development of  order && orderItems api business logic

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,14 +6,14 @@ const Definer = require("../lib/mistake");
 let orderController = module.exports;
 orderController.createOrder = async (req, res) => {
   try {
-    console.log("POST: cont/signup");
+    console.log("POST: cont/sigcreateOrdernup");
     assert.ok(req.member, Definer.auth_err5);
     const order = new Order();
     const result = await order.createOrderData(req.member,req.body);
     console.log(req.body );
     res.json({ state: "succses", data: result });
   } catch (err) {
-    console.log(`ERROR: cont/signup,${err.message}`);
+    console.log(`ERROR: cont/createOrder,${err.message}`);
     res.json({ state: "fail", message: err.message });
   }
 };
