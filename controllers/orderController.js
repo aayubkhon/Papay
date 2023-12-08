6298a08e1724d4abcb7e8e43c74dd6bd6a39b239
aayubxon701 <aayubxon701@gmail.com>
2023-12-09 05:16:43 +0900

    feat:order creation part one

---
+++ controllers/orderController.js
@@ -0,0 +1,19 @@
+const Order = require("../models/Order");
+
+const assert = require("assert");
+const Definer = require("../lib/mistake");
+
+let orderController = module.exports;
+orderController.createOrder = async (req, res) => {
+  try {
+    console.log("POST: cont/signup");
+    assert.ok(req.member, Definer.auth_err5);
+    const order = new Order();
+    const result = await order.createOrderData(req.member,req.body);
+    console.log(req.body );
+    res.json({ state: "succses", data: result });
+  } catch (err) {
+    console.log(`ERROR: cont/signup,${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const memberController = require("./controllers/memberController");
 const productController = require("./controllers/productController");
 const restaurantController = require("./controllers/restaurantController");
+const orderController = require("./controllers/orderController");
 
 /****************************
  *         REST API/       *
@@ -34,6 +35,8 @@ router.get(
   productController.getChosenProduct
 );
 
+// Restaurants related routers
+
 router.get(
   "/restaurants",
   memberController.retrieveAuthMember,
@@ -44,5 +47,12 @@ router.get(
   "/restaurants/:id",
   memberController.retrieveAuthMember,
   restaurantController.getChosenRestaurant
+);
+
+// Order related routers
+router.post(
+  "/orders/create",
+  memberController.retrieveAuthMember,
+  orderController.createOrder
 );
 module.exports = router;
